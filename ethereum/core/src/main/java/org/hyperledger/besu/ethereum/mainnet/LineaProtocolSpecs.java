09dd64ae70f5ea0390978c5323e505150d697bc3
Gabriel-Trintinalia <gabriel.trintinalia@consensys.net>
2023-05-04 12:06:34 +1000

    Override evm operations with Linea behaviours

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/LineaProtocolSpecs.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/mainnet/LineaProtocolSpecs.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright Hyperledger Besu Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.besu.ethereum.mainnet;
 
 import org.hyperledger.besu.config.GenesisConfigOptions;
@@ -6,6 +20,7 @@ import org.hyperledger.besu.ethereum.linea.LineaBlockBodyValidator;
 import org.hyperledger.besu.ethereum.linea.LineaParameters;
 import org.hyperledger.besu.ethereum.linea.LineaTransactionValidator;
 import org.hyperledger.besu.ethereum.mainnet.feemarket.FeeMarket;
+import org.hyperledger.besu.evm.MainnetEVMs;
 import org.hyperledger.besu.evm.internal.EvmConfiguration;
 import org.hyperledger.besu.plugin.data.TransactionType;
 
@@ -62,6 +77,10 @@ public class LineaProtocolSpecs {
                         TransactionType.EIP1559),
                     txCalldataMaxSize))
         .blockBodyValidatorBuilder(LineaBlockBodyValidator::new)
+        // some Linea evm opcodes behave differently.
+        .evmBuilder(
+            (gasCalculator, jdCacheConfig) ->
+                MainnetEVMs.linea(gasCalculator, chainId.orElse(BigInteger.ZERO), evmConfiguration))
         .name("Linea");
   }
 }
